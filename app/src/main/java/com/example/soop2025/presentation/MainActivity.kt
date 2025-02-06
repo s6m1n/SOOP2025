da2b12dcd16b8b0498b588e49ca00fa3afbaf71d
somin <salth0601@gmail.com>
2025-02-07 02:01:15 +0900

    feat: Application 클래스 추가

--- app/src/main/java/com/example/soop2025/presentation/MainActivity.kt
+++ app/src/main/java/com/example/soop2025/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.soop2025.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val reposSearchViewModel by viewModels<ReposSearchViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +26,8 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val navController = rememberNavController()
                     MainNavHost(
-                        navController
+                        navController,
+                        reposSearchViewModel
                     )
                 }
             }
