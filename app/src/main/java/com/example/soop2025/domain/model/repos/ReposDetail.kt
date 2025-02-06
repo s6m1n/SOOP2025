b96022e66d4efa475e731bfefd5df4dcf652a981
somin <salth0601@gmail.com>
2025-02-07 02:45:19 +0900

    feat: 깃허브 저장소 정보 조회를 위한 도메인 객체 클래스 추가

---
+++ app/src/main/java/com/example/soop2025/domain/model/repos/ReposDetail.kt
@@ -0,0 +1,13 @@
+package com.example.soop2025.domain.model.repos
+
+data class ReposDetail(
+    val id: Long,
+    val repoName: String,
+    val description: String?,
+    val starCount: Int,
+    val watchersCount: Int,
+    val forksCount: Int,
+    val language: String?,
+    val userName: String,
+    val userProfileImageUrl: String
+)
