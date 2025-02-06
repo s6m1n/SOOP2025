cb435ba91753acf3b5db7fddbe82a2f43aa84a9f
somin <salth0601@gmail.com>
2025-02-07 01:52:15 +0900

    feat: ReposSearchScreen ui 구현

--- app/src/main/java/com/example/soop2025/presentation/ui/screen/ReposSearchScreen.kt
+++ app/src/main/java/com/example/soop2025/presentation/ui/screen/ReposSearchScreen.kt
@@ -1,7 +1,37 @@
 package com.example.soop2025.presentation.ui.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import com.example.soop2025.presentation.ReposSearchViewModel
+import com.example.soop2025.presentation.ui.component.SearchLazyColumn
+import com.example.soop2025.presentation.ui.component.SearchTextField
 
 @Composable
-fun ReposSearchScreen() {
+fun ReposSearchScreen(
+    reposSearchViewModel: ReposSearchViewModel,
+    onMoveReposDetailScreen: (String, String) -> Unit
+) {
+    var searchValue by rememberSaveable { mutableStateOf("") }
+
+    Column {
+        SearchTextField(
+            value = searchValue,
+            onValueChange = {
+                searchValue = it
+            },
+            onSearchDone = {
+                reposSearchViewModel.searchReposBy(searchValue)
+            }
+        )
+        SearchLazyColumn(
+            reposSearchViewModel.searchResult.collectAsState().value
+        ) { userName, repoName ->
+            onMoveReposDetailScreen(userName, repoName)
+        }
+    }
 }
