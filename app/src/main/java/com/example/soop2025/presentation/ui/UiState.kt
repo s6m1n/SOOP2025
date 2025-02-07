ce19889114eebbc03d25bebd5c6dca1f61792a6b
somin <salth0601@gmail.com>
2025-02-07 13:22:42 +0900

    feat: UiState sealed interface 추가

---
+++ app/src/main/java/com/example/soop2025/presentation/ui/UiState.kt
@@ -0,0 +1,7 @@
+package com.example.soop2025.presentation.ui
+sealed interface UiState<out T> {
+    data object Idle : UiState<Nothing>
+    data object Loading : UiState<Nothing>
+    data class Success<out T>(val data: T) : UiState<T>
+    data class Error(val message: String) : UiState<Nothing>
+}
