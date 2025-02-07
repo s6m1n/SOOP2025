47fd0d87caa21539373a00a38f349077c3fe55f6
somin <salth0601@gmail.com>
2025-02-07 13:06:33 +0900

    feat: 로딩 상태를 위한 CircularLoading 컴포넌트 구현

---
+++ app/src/main/java/com/example/soop2025/presentation/ui/component/CircularLoading.kt
@@ -0,0 +1,33 @@
+package com.example.soop2025.presentation.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularLoading(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            modifier = Modifier
+                .width(40.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CircularLoadingPreview() {
+    CircularLoading()
+}
