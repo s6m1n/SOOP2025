4714c9433870973a1402175725bd1eaf34bcb7ca
somin <salth0601@gmail.com>
2025-02-09 00:55:36 +0900

    feat: ReposScreen에 HandleErrorUiState 적용

--- app/src/main/java/com/example/soop2025/presentation/ui/screen/ReposScreen.kt
+++ app/src/main/java/com/example/soop2025/presentation/ui/screen/ReposScreen.kt
@@ -1,5 +1,6 @@
 package com.example.soop2025.presentation.ui.screen
 
+import android.widget.Toast
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -8,6 +9,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.soop2025.domain.model.repos.Repos
@@ -26,16 +28,16 @@ fun ReposScreen(
 ) {
     LaunchedEffect(key1 = Unit) { reposViewModel.fetchRepos(userName, repoName) }
 
-    when (val state = reposViewModel.reposState.collectAsStateWithLifecycle().value) {
+    when (val reposState = reposViewModel.reposState.collectAsStateWithLifecycle().value) {
         ReposUiState.Idle -> {}
         ReposUiState.Loading -> CircularLoading()
         is ReposUiState.Success -> HandleSuccessUiState(
-            state.data,
+            reposState.data,
             userName,
             reposViewModel
         )
 
-        is ReposUiState.Error -> { }
+        is ReposUiState.Error -> { HandleErrorUiState(reposState.message) }
     }
 }
 
@@ -68,5 +70,15 @@ private fun HandleSuccessUiState(
             }
             is UserUiState.Error -> {}
         }
+    }
+}
+
+@Composable
+fun HandleErrorUiState(
+    message: String
+) {
+    val currentContext = LocalContext.current
+    LaunchedEffect(Unit) {
+        Toast.makeText(currentContext, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/soop2025/presentation/ui/user/UserBottomSheet.kt
+++ app/src/main/java/com/example/soop2025/presentation/ui/user/UserBottomSheet.kt
@@ -1,13 +1,11 @@
 package com.example.soop2025.presentation.ui.user
 
-import android.widget.Toast
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
 import com.example.soop2025.presentation.ui.UserUiState
 import com.example.soop2025.presentation.ui.component.CircularLoading
+import com.example.soop2025.presentation.ui.screen.HandleErrorUiState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,10 +27,7 @@ fun UserBottomSheet(
             userName
         )
         is UserUiState.Error -> {
-            val context = LocalContext.current
-            LaunchedEffect(Unit) {
-                Toast.makeText(context, userState.message, Toast.LENGTH_SHORT).show()
-            }
+            HandleErrorUiState(userState.message)
         }
     }
 }
