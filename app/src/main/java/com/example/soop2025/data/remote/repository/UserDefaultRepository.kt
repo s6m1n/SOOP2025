af7f8fd18ce458dd8f841127f9bce82452622b02
somin <salth0601@gmail.com>
2025-02-08 17:57:32 +0900

    feat: UserRepository에서 fetchUserDetail와 fetchUserRepos 통합

--- app/src/main/java/com/example/soop2025/data/remote/repository/UserDefaultRepository.kt
+++ app/src/main/java/com/example/soop2025/data/remote/repository/UserDefaultRepository.kt
@@ -6,10 +6,9 @@ import com.example.soop2025.data.remote.ResponseResult.Exception
 import com.example.soop2025.data.remote.ResponseResult.Success
 import com.example.soop2025.data.remote.api.UserApiService
 import com.example.soop2025.data.remote.model.response.mapper.toUserDetail
-import com.example.soop2025.data.remote.model.response.mapper.toUserRepos
+import com.example.soop2025.data.remote.model.response.mapper.toUserRepository
 import com.example.soop2025.domain.UserRepository
 import com.example.soop2025.domain.model.user.UserDetail
-import com.example.soop2025.domain.model.user.UserRepositories
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -17,25 +16,25 @@ import javax.inject.Inject
 class UserDefaultRepository @Inject constructor(
     private val userApiService: UserApiService
 ) : UserRepository {
-    override suspend fun fetchUserDetail(userName: String): Flow<ResponseResult<UserDetail>> {
-        val responseResult = handleApiResponse { userApiService.getUserDetail(userName) }
-        return flow {
-            emit(
-                when (responseResult) {
-                    is Success -> Success(responseResult.data.toUserDetail())
-                    is Exception -> Exception(responseResult.e, responseResult.message)
-                }
-            )
-        }
-    }
 
-    override suspend fun fetchUserRepos(userName: String): Flow<ResponseResult<UserRepositories>> {
-        val responseResult = handleApiResponse { userApiService.getUserRepos(userName) }
+    override suspend fun fetchUserDetail(userName: String): Flow<ResponseResult<UserDetail>> {
+        val responseUserResult = handleApiResponse { userApiService.getUser(userName) }
+        val responseRepoResult = handleApiResponse { userApiService.getUserRepos(userName) }
+
         return flow {
             emit(
-                when (responseResult) {
-                    is Success -> Success(responseResult.data.toUserRepos())
-                    is Exception -> Exception(responseResult.e, responseResult.message)
+                if (responseUserResult is Success && responseRepoResult is Success) {
+                    Success(responseUserResult.data.toUserDetail(
+                        repositories = responseRepoResult.data.toUserRepository()
+                    ))
+                } else {
+                    if (responseUserResult is Exception) {
+                        Exception(responseUserResult.e, responseUserResult.message)
+                    } else if (responseRepoResult is Exception) {
+                        Exception(responseRepoResult.e, responseRepoResult.message)
+                    } else {
+                        Exception(RuntimeException(), "server fail exception")
+                    }
                 }
             )
         }
--- app/src/main/java/com/example/soop2025/domain/UserRepository.kt
+++ app/src/main/java/com/example/soop2025/domain/UserRepository.kt
@@ -2,11 +2,8 @@ package com.example.soop2025.domain
 
 import com.example.soop2025.data.remote.ResponseResult
 import com.example.soop2025.domain.model.user.UserDetail
-import com.example.soop2025.domain.model.user.UserRepositories
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     suspend fun fetchUserDetail(userName: String): Flow<ResponseResult<UserDetail>>
-
-    suspend fun fetchUserRepos(userName: String): Flow<ResponseResult<UserRepositories>>
 }
