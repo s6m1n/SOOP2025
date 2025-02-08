9d300d54baaefed9d5955e67234adc25a62ad0f5
somin <salth0601@gmail.com>
2025-02-08 18:19:33 +0900

    refactor: fetchUserDetail 내부의 네트워크 요청들을 각각 비동기 처리

--- app/src/main/java/com/example/soop2025/data/remote/repository/UserDefaultRepository.kt
+++ app/src/main/java/com/example/soop2025/data/remote/repository/UserDefaultRepository.kt
@@ -7,8 +7,12 @@ import com.example.soop2025.data.remote.ResponseResult.Success
 import com.example.soop2025.data.remote.api.UserApiService
 import com.example.soop2025.data.remote.model.response.mapper.toUserDetail
 import com.example.soop2025.data.remote.model.response.mapper.toUserRepository
+import com.example.soop2025.data.remote.model.response.user.UserReposResponse
+import com.example.soop2025.data.remote.model.response.user.UserResponse
 import com.example.soop2025.domain.UserRepository
 import com.example.soop2025.domain.model.user.UserDetail
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -17,26 +21,38 @@ class UserDefaultRepository @Inject constructor(
     private val userApiService: UserApiService
 ) : UserRepository {
 
-    override suspend fun fetchUserDetail(userName: String): Flow<ResponseResult<UserDetail>> {
-        val responseUserResult = handleApiResponse { userApiService.getUser(userName) }
-        val responseRepoResult = handleApiResponse { userApiService.getUserRepos(userName) }
+    override suspend fun fetchUserDetail(userName: String): Flow<ResponseResult<UserDetail>> =
+        flow {
+            coroutineScope {
+                val userDeferred =
+                    async { handleApiResponse { userApiService.getUser(userName) } }
+                val userReposDeferred =
+                    async { handleApiResponse { userApiService.getUserRepos(userName) } }
 
-        return flow {
-            emit(
-                if (responseUserResult is Success && responseRepoResult is Success) {
-                    Success(responseUserResult.data.toUserDetail(
-                        repositories = responseRepoResult.data.toUserRepository()
-                    ))
+                val userResult = userDeferred.await()
+                val userReposResult = userReposDeferred.await()
+
+                if (userResult is Success && userReposResult is Success) {
+                    emit(Success(userResult.data.toUserDetail(userReposResult.data.toUserRepository())))
+                    return@coroutineScope
                 } else {
-                    if (responseUserResult is Exception) {
-                        Exception(responseUserResult.e, responseUserResult.message)
-                    } else if (responseRepoResult is Exception) {
-                        Exception(responseRepoResult.e, responseRepoResult.message)
-                    } else {
-                        Exception(RuntimeException(), "server fail exception")
-                    }
+                    emit(handleExceptionResponse(userResult, userReposResult))
+                    return@coroutineScope
                 }
+            }
+        }
+
+    private fun handleExceptionResponse(
+        userResult: ResponseResult<UserResponse>,
+        userReposResult: ResponseResult<List<UserReposResponse>>
+    ): Exception<UserDetail> {
+        return when {
+            userResult is Exception -> Exception(userResult.e, userResult.message)
+            userReposResult is Exception -> Exception(
+                userReposResult.e,
+                userReposResult.message
             )
+            else -> Exception(RuntimeException(), "Server failure")
         }
     }
 }
